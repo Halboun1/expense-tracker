a6b18d7bb4e21ed72bbf17ca13de677b4e340588
mohamed halbouni <halbounicd@gmail.com>
2024-11-26 20:43:35 -0500

    creating the accountController, account can be created or amount can be inserted to it

--- backend/routes/accountRoutes.js
+++ backend/routes/accountRoutes.js
@@ -4,12 +4,12 @@ import {
     createAccount,
     getAccounts,
 } from "../controllers/accountController.js";
-import authMiddleware from "../middlewares/authMiddleware.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/:id?", authMiddleware, getAccounts)
 router.post("/create", authMiddleware, createAccount);
-router.put("/add-money", authMiddleware, addMoneyToAccount);
+router.put("/add-money/:id", authMiddleware, addMoneyToAccount);
 
 export default router;
